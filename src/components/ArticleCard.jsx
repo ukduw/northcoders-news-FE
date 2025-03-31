f084e7cf36026768011d8afe84a660f101a67d0f
ukduw <edhkmayo@gmail.com>
2025-03-31 15:53:35 +0100

    all articles; not flex/clickable

---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,25 @@
+
+function ArticleCard({articles}) {
+    const articleArr = articles.articles
+
+    return (
+        <div className="articles-grid">
+            {articleArr.map((article) => {
+                return <div key={article.article_id} className="article-card">
+                    <img src={article.article_img_url} alt={article.title} className="article-img"></img>
+                    <div className="article-details">
+                        <h2>{article.title}</h2>
+                        <p>{article.topic}</p>
+                        <p>{article.author}</p>
+                        <p>{article.created_at}</p>
+                        <p>Votes: {article.votes}</p>
+                        <p>Comments: {article.comment_count}</p>
+                    </div>
+                </div>
+            })}
+        </div>
+    )
+}
+
+
+export default ArticleCard
