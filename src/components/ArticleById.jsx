0bd07dbf5ddaa236719a6d040314b41bea285195
ukduw <edhkmayo@gmail.com>
2025-04-01 10:21:22 +0100

    4-comments

--- src/components/ArticleById.jsx
+++ src/components/ArticleById.jsx
@@ -1,10 +1,11 @@
-import { getArticleById } from "../../api"
+import { getArticleById, getCommentsByArticleId } from "../../api"
 import ArticleByIdCard from "./ArticleByIdCard"
 import {useState, useEffect} from "react"
 import {useParams} from "react-router-dom"
 
 function ArticleById() {
     const [article, setArticle] = useState('')
+    const [comments, setComments] = useState([])
     const [loading, setLoading] = useState(true)
     const {article_id} = useParams()
     
@@ -13,12 +14,15 @@ function ArticleById() {
             setArticle(article)
             setLoading(false)
         })
+        getCommentsByArticleId(article_id).then((comments) => {
+            setComments(comments)
+        })
     }, [article_id])
 
     return (
         <div>
             {loading ? <p className="loading-message">Article Loading...</p> : 
-                <ArticleByIdCard article={article} />
+                <ArticleByIdCard article={article} comments={comments} />
             }
         </div>
     )
