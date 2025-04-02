a77830f83c01a4b59a2d7d113446b97b7b4e16b9
ukduw <edhkmayo@gmail.com>
2025-04-02 15:38:40 +0100

    9-sort_by

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
 function Header() {
+    const navigate = useNavigate()
+
+    function handleHomeClick() {
+        navigate('/')
+    }
+
     return (
-        <h1>HEADER TEST</h1>
+        <section>
+            <h1>HEADER TEST</h1>
+            <img src="https://png.pngtree.com/element_our/sm/20180516/sm_5afc76eed0142.jpg" alt="home button" onClick={handleHomeClick} className="home-button"/>
+        </section>
     )
 }
 
